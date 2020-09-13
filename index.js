36c66cab11c67f40eb1d0fa0b9bdf73fc57acd68
Mahiler <fernando.mahiler@gmail.com>
2020-09-13 12:57:21 -0500

    socket reload

--- index.js
+++ index.js
@@ -13,19 +13,13 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("New client connected " + socket.id);
-
-  socket.on("notification", (data) => {
-    if (interval) {
-      clearInterval(interval);
-    }
-    interval = setInterval(() => getDniAndEmit(socket, data), 1000);
+  socket.on("admin", (data) => {
+    if (interval) clearInterval(interval);
+    interval = setInterval(() => getter(socket, data, true), 1000);
   });
-
-  socket.on("getUsers", (jwt) => {
-    if (interval) {
-      clearInterval(interval);
-    }
-    interval = setInterval(() => getUsersAndEmit(socket, jwt), 1000);
+  socket.on("patient", (data) => {
+    if (interval) clearInterval(interval);
+    interval = setInterval(() => getter(socket, data), 1000);
   });
   socket.on("disconnect", () => {
     console.log("Client disconnected");
@@ -33,36 +27,38 @@ io.on("connection", (socket) => {
   });
 });
 
-const getUsersAndEmit = (socket, jwt) => {
-  axios
-    .get("https://siama-node-js.herokuapp.com/v1/api/user", {
+const getter = async (socket, data, flag = false) => {
+  const newData = {};
+  let user = await getDniAndEmit(data);
+  newData.user = user;
+  if (flag) {
+    newData.users = await getUsersAndEmit(data.jwt);
+  }
+  socket.emit("getData", newData);
+};
+
+const getUsersAndEmit = async (jwt) => {
+  let users = await axios.get(
+    "https://siama-node-js.herokuapp.com/v1/api/user",
+    {
       headers: {
         Authorization: jwt,
       },
-    })
-    .then((result) => {
-      socket.emit("recibeUser", result.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    },
+  );
+  return users.data;
 };
 
-const getDniAndEmit = (socket, data) => {
-  console.log(data);
-
-  axios
-    .get(`https://siama-node-js.herokuapp.com/v1/api/user/dni/${data.user}`, {
+const getDniAndEmit = async (data) => {
+  let user = await axios.get(
+    `https://siama-node-js.herokuapp.com/v1/api/user/dni/${data.user}`,
+    {
       headers: {
         Authorization: data.jwt,
       },
-    })
-    .then((result) => {
-      socket.emit("recibeUserDni", result.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    },
+  );
+  return user.data;
 };
 
 server.listen(PORT, () => {
