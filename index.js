c8f7319b3db3b0562d5493af1d5de6e67ec2b597
Mahiler <fernando.mahiler@gmail.com>
2020-09-19 23:19:06 -0500

    time

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ io.on("connection", (socket) => {
   console.log("New client connected " + socket.id);
   socket.on("admin", (data) => {
     if (interval) clearInterval(interval);
-    interval = setInterval(() => getter(socket, data, true), 1500);
+    interval = setInterval(() => getter(socket, data, true), 5000);
   });
   socket.on("patient", (data) => {
     if (interval) clearInterval(interval);
-    interval = setInterval(() => getter(socket, data), 1500);
+    interval = setInterval(() => getter(socket, data), 2000);
   });
   socket.on("disconnect", () => {
     console.log("Client disconnected");
