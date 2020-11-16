91faca8dc2aca496fa9f5a19666c8cc89266ee42
Mahiler <fernando.mahiler@gmail.com>
2020-11-15 23:04:33 -0500

    asdas

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ io.on("connection", (socket) => {
 
   socket.on("sendMessage", async (data) => {
     console.log(data);
+    let re;
     try {
       let receptor = await axios
         .get(`${BASE_URL}/user/${data.receptorId}`, {
@@ -59,20 +60,25 @@ io.on("connection", (socket) => {
           },
         })
         .then(async (receptor) => {
-          console.log(receptor);
           await axios.post(`${BASE_URL}/messages`, {
-            idPatient: data.role === "patient" ? data.userId : receptor._id,
-            idMedical: data.role === "patient" ? receptor._id : data.userId,
+            idPatient: data.role === "patient" ? data.userId : data.receptorId,
+            idMedical: data.role === "patient" ? data.receptorId : data.userId,
             message: data.message,
             send: data.role === "patient" ? 1 : 0,
           });
+          re = receptor;
+          return receptor;
         });
+      console.log("---------");
+      console.log(receptor);
+      console.log("---------");
+      console.log(re);
+      console.log("---------");
       if (receptor.socketId) {
         socket.broadcast.to(receptor.socketId).emit("recibeMessage", { message: data.message, idEmisor: data.userId, date: new Date() });
       }
     } catch (error) {
       console.log("ERROR DE SENDMESSAGE");
-      console.log(error);
     }
   });
 
